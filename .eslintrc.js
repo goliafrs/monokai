df3f203fcb88e01c0e705426741a9dee784ef9e4
GoliafRS <goliaf.rs@gmail.com>
2020-03-10 11:57:47 +0300

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,20 +12,98 @@ module.exports = {
   },
   rules: {
     semi: ['error', 'never'],
+    'no-extra-semi': 'error',
+
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+        VariableDeclarator: 'first'
+      }
+    ],
+
+    'no-trailing-spaces': 'error',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+
+    // comma
+    'comma-style': ['error', 'last'],
+    'comma-dangle': ['error', 'never'],
+    'comma-spacing': [
+      'error',
+      {
+        before: false,
+        after: true
+      }
+    ],
+
+    'brace-style': 'error',
+    'block-spacing': 'error',
+    'keyword-spacing': [
+      'error',
+      {
+        before: true,
+        after: true
+      }
+    ],
+    'require-atomic-updates': 'off',
+    'no-useless-catch': 'off',
+
+    // functions
+    'function-paren-newline': ['error', 'consistent'],
+    'func-call-spacing': ['error', 'never'],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'never',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ],
+
+    // objects
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: {
+          consistent: true,
+          multiline: true,
+          minProperties: 2
+        },
+        ObjectPattern: {
+          consistent: true,
+          multiline: true
+        },
+        ImportDeclaration: {
+          consistent: true,
+          multiline: true
+        },
+        ExportDeclaration: {
+          consistent: true,
+          multiline: true
+        }
+      }
+    ],
+    'object-curly-spacing': ['error', 'always'],
+    'object-property-newline': ['error', { allowAllPropertiesOnSameLine: false }],
+    'no-whitespace-before-property': 'error',
+
+    // arrays
+    'array-bracket-newline': ['error', { multiline: true }],
+    'array-bracket-spacing': ['error', 'always'],
+    'array-element-newline': ['error', 'consistent'],
+
+    // lines
+    'linebreak-style': ['error', 'unix'],
+    'lines-between-class-members': ['error', 'always'],
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+
+    // comments
+    'spaced-comment': ['error', 'always'],
+
+    // quotes
     quotes: ['error', 'single'],
     'quote-props': ['error', 'as-needed'],
-    indent: ['error', 2, { SwitchCase: 1 }],
-    'function-paren-newline': ['error', 'consistent'],
-    'object-property-newline': ['error', { allowAllPropertiesOnSameLine: true }],
-    'object-curly-newline': ['error', { consistent: true, minProperties: 2 }],
-    'object-curly-spacing': ['error', 'always'],
-    'array-bracket-newline': ['error', 'consistent'],
-    'array-bracket-spacing': ['error', 'always'],
-    'array-element-newline': ['error', { minItems: 2 }],
-    'linebreak-style': ['error', 'unix'],
-    'jsx-quotes': ['error', 'prefer-single'],
-    'no-console': ['error', { allow: ['warn', 'error'] }],
-    'comma-dangle': ['error', 'never'],
-    'brace-style': 'error'
+    'jsx-quotes': ['error', 'prefer-single']
   }
 }
